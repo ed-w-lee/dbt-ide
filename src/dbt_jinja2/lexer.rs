43d5a2eb53eeb9007efcda2f2bc0bcd62e089f86
Edward Lee <edwardlee.259@gmail.com>
2022-01-15 16:14:27 -0800

    I think this is the lexer

--- src/dbt_jinja2/lexer.rs
+++ src/dbt_jinja2/lexer.rs
@@ -1,4 +1,4 @@
-use fancy_regex::{escape, Regex};
+use fancy_regex::{escape, Match, Regex};
 use lazy_static::lazy_static;
 use std::collections::{HashMap, VecDeque};
 
@@ -48,9 +48,7 @@ enum TokenKind {
     VariableEnd,   // "}}" can include -
     Comment,
     Data,
-    Initial,
-    EOF,
-    Root,
+    Error,
 }
 
 #[derive(Debug)]
@@ -61,13 +59,12 @@ struct Token {
 
 lazy_static! {
     // Regexes copied from Jinja's lexer logic
-    static ref WHITESPACE_RE: Regex = Regex::new(r"^\s+").unwrap();
-    static ref NEWLINE_RE: Regex = Regex::new(r"^(\r\n|\r|\n)").unwrap();
+    static ref WHITESPACE_RE: Regex = Regex::new(r"\s+").unwrap();
+    static ref NEWLINE_RE: Regex = Regex::new(r"(\r\n|\r|\n)").unwrap();
     static ref STRING_RE: Regex =
-        Regex::new(r#"(?s)^('([^'\\]*(?:\\.[^'\\]*)*)'" r'|"([^"\\]*(?:\\.[^"\\]*)*)")"#).unwrap();
+        Regex::new(r#"(?s)('([^'\\]*(?:\\.[^'\\]*)*)'" r'|"([^"\\]*(?:\\.[^"\\]*)*)")"#).unwrap();
     static ref INTEGER_RE: Regex = Regex::new(
         r#"(?ix)
-        ^
         (
             0b(_?[0-1])+    # binary
         |
@@ -84,9 +81,7 @@ lazy_static! {
     .unwrap();
     static ref FLOAT_RE: Regex = Regex::new(
         r#"(?ix)
-        ^
-        # TODO: CHECK IN APPLICATION LOGIC
-        # (?<!\.)                 # doesn't start with a . (for "tuple.0.0")
+        (?<!\.)                 # doesn't start with a . (for "tuple.0.0")
         (\d+_)*\d+              # digits, possibly _ separated
         (
             (\.(\d+_)*\d+)?     # optional fractional part
@@ -99,7 +94,7 @@ lazy_static! {
     .unwrap();
     // Copy-pasted from https://github.com/pallets/jinja/blob/4a33989236671e3a1b78718bc45d890616a4f75e/src/jinja2/_identifier.py
     static ref NAME_RE: Regex = Regex::new(
-        r#"^[\w·̀-ͯ·҃-֑҇-ׇֽֿׁׂׅׄؐ-ًؚ-ٰٟۖ-ۜ۟-۪ۤۧۨ-ܑۭܰ-݊ަ-ް߫-߽߳ࠖ-࠙ࠛ-ࠣࠥ-ࠧࠩ-࡙࠭-࡛࣓-ࣣ࣡-ःऺ-़ा-ॏ॑-ॗॢॣঁ-ঃ়া-ৄেৈো-্ৗৢৣ৾ਁ-ਃ਼ਾ-ੂੇੈੋ-੍ੑੰੱੵઁ-ઃ઼ા-ૅે-ૉો-્ૢૣૺ-૿ଁ-ଃ଼ା-ୄେୈୋ-୍ୖୗୢୣஂா-ூெ-ைொ-்ௗఀ-ఄా-ౄె-ైొ-్ౕౖౢౣಁ-ಃ಼ಾ-ೄೆ-ೈೊ-್ೕೖೢೣഀ-ഃ഻഼ാ-ൄെ-ൈൊ-്ൗൢൣංඃ්ා-ුූෘ-ෟෲෳัิ-ฺ็-๎ັິ-ູົຼ່-ໍ༹༘༙༵༷༾༿ཱ-྄྆྇ྍ-ྗྙ-ྼ࿆ါ-ှၖ-ၙၞ-ၠၢ-ၤၧ-ၭၱ-ၴႂ-ႍႏႚ-ႝ፝-፟ᜒ-᜔ᜲ-᜴ᝒᝓᝲᝳ឴-៓៝᠋-᠍ᢅᢆᢩᤠ-ᤫᤰ-᤻ᨗ-ᨛᩕ-ᩞ᩠-᩿᩼᪰-᪽ᬀ-ᬄ᬴-᭄᭫-᭳ᮀ-ᮂᮡ-ᮭ᯦-᯳ᰤ-᰷᳐-᳔᳒-᳨᳭ᳲ-᳴᳷-᳹᷀-᷹᷻-᷿‿⁀⁔⃐-⃥⃜⃡-⃰℘℮⳯-⵿⳱ⷠ-〪ⷿ-゙゚〯꙯ꙴ-꙽ꚞꚟ꛰꛱ꠂ꠆ꠋꠣ-ꠧꢀꢁꢴ-ꣅ꣠-꣱ꣿꤦ-꤭ꥇ-꥓ꦀ-ꦃ꦳-꧀ꧥꨩ-ꨶꩃꩌꩍꩻ-ꩽꪰꪲ-ꪴꪷꪸꪾ꪿꫁ꫫ-ꫯꫵ꫶ꯣ-ꯪ꯬꯭ﬞ︀-️︠-︯︳︴﹍-﹏＿𐇽𐋠𐍶-𐍺𐨁-𐨃𐨅𐨆𐨌-𐨏𐨸-𐨿𐨺𐫦𐫥𐴤-𐽆𐴧-𐽐𑀀-𑀂𑀸-𑁆𑁿-𑂂𑂰-𑂺𑄀-𑄂𑄧-𑄴𑅅𑅆𑅳𑆀-𑆂𑆳-𑇀𑇉-𑇌𑈬-𑈷𑈾𑋟-𑋪𑌀-𑌃𑌻𑌼𑌾-𑍄𑍇𑍈𑍋-𑍍𑍗𑍢𑍣𑍦-𑍬𑍰-𑍴𑐵-𑑆𑑞𑒰-𑓃𑖯-𑖵𑖸-𑗀𑗜𑗝𑘰-𑙀𑚫-𑚷𑜝-𑜫𑠬-𑠺𑨁-𑨊𑨳-𑨹𑨻-𑨾𑩇𑩑-𑩛𑪊-𑪙𑰯-𑰶𑰸-𑰿𑲒-𑲧𑲩-𑲶𑴱-𑴶𑴺𑴼𑴽𑴿-𑵅𑵇𑶊-𑶎𑶐𑶑𑶓-𑶗𑻳-𑻶𖫰-𖫴𖬰-𖬶𖽑-𖽾𖾏-𖾒𛲝𛲞𝅥-𝅩𝅭-𝅲𝅻-𝆂𝆅-𝆋𝆪-𝆭𝉂-𝉄𝨀-𝨶𝨻-𝩬𝩵𝪄𝪛-𝪟𝪡-𝪯𞀀-𞀆𞀈-𞀘𞀛-𞀡𞀣𞀤𞀦-𞣐𞀪-𞣖𞥄-𞥊󠄀-󠇯]+"#
+        r#"[\w·̀-ͯ·҃-֑҇-ׇֽֿׁׂׅׄؐ-ًؚ-ٰٟۖ-ۜ۟-۪ۤۧۨ-ܑۭܰ-݊ަ-ް߫-߽߳ࠖ-࠙ࠛ-ࠣࠥ-ࠧࠩ-࡙࠭-࡛࣓-ࣣ࣡-ःऺ-़ा-ॏ॑-ॗॢॣঁ-ঃ়া-ৄেৈো-্ৗৢৣ৾ਁ-ਃ਼ਾ-ੂੇੈੋ-੍ੑੰੱੵઁ-ઃ઼ા-ૅે-ૉો-્ૢૣૺ-૿ଁ-ଃ଼ା-ୄେୈୋ-୍ୖୗୢୣஂா-ூெ-ைொ-்ௗఀ-ఄా-ౄె-ైొ-్ౕౖౢౣಁ-ಃ಼ಾ-ೄೆ-ೈೊ-್ೕೖೢೣഀ-ഃ഻഼ാ-ൄെ-ൈൊ-്ൗൢൣංඃ්ා-ුූෘ-ෟෲෳัิ-ฺ็-๎ັິ-ູົຼ່-ໍ༹༘༙༵༷༾༿ཱ-྄྆྇ྍ-ྗྙ-ྼ࿆ါ-ှၖ-ၙၞ-ၠၢ-ၤၧ-ၭၱ-ၴႂ-ႍႏႚ-ႝ፝-፟ᜒ-᜔ᜲ-᜴ᝒᝓᝲᝳ឴-៓៝᠋-᠍ᢅᢆᢩᤠ-ᤫᤰ-᤻ᨗ-ᨛᩕ-ᩞ᩠-᩿᩼᪰-᪽ᬀ-ᬄ᬴-᭄᭫-᭳ᮀ-ᮂᮡ-ᮭ᯦-᯳ᰤ-᰷᳐-᳔᳒-᳨᳭ᳲ-᳴᳷-᳹᷀-᷹᷻-᷿‿⁀⁔⃐-⃥⃜⃡-⃰℘℮⳯-⵿⳱ⷠ-〪ⷿ-゙゚〯꙯ꙴ-꙽ꚞꚟ꛰꛱ꠂ꠆ꠋꠣ-ꠧꢀꢁꢴ-ꣅ꣠-꣱ꣿꤦ-꤭ꥇ-꥓ꦀ-ꦃ꦳-꧀ꧥꨩ-ꨶꩃꩌꩍꩻ-ꩽꪰꪲ-ꪴꪷꪸꪾ꪿꫁ꫫ-ꫯꫵ꫶ꯣ-ꯪ꯬꯭ﬞ︀-️︠-︯︳︴﹍-﹏＿𐇽𐋠𐍶-𐍺𐨁-𐨃𐨅𐨆𐨌-𐨏𐨸-𐨿𐨺𐫦𐫥𐴤-𐽆𐴧-𐽐𑀀-𑀂𑀸-𑁆𑁿-𑂂𑂰-𑂺𑄀-𑄂𑄧-𑄴𑅅𑅆𑅳𑆀-𑆂𑆳-𑇀𑇉-𑇌𑈬-𑈷𑈾𑋟-𑋪𑌀-𑌃𑌻𑌼𑌾-𑍄𑍇𑍈𑍋-𑍍𑍗𑍢𑍣𑍦-𑍬𑍰-𑍴𑐵-𑑆𑑞𑒰-𑓃𑖯-𑖵𑖸-𑗀𑗜𑗝𑘰-𑙀𑚫-𑚷𑜝-𑜫𑠬-𑠺𑨁-𑨊𑨳-𑨹𑨻-𑨾𑩇𑩑-𑩛𑪊-𑪙𑰯-𑰶𑰸-𑰿𑲒-𑲧𑲩-𑲶𑴱-𑴶𑴺𑴼𑴽𑴿-𑵅𑵇𑶊-𑶎𑶐𑶑𑶓-𑶗𑻳-𑻶𖫰-𖫴𖬰-𖬶𖽑-𖽾𖾏-𖾒𛲝𛲞𝅥-𝅩𝅭-𝅲𝅻-𝆂𝆅-𝆋𝆪-𝆭𝉂-𝉄𝨀-𝨶𝨻-𝩬𝩵𝪄𝪛-𝪟𝪡-𝪯𞀀-𞀆𞀈-𞀘𞀛-𞀡𞀣𞀤𞀦-𞣐𞀪-𞣖𞥄-𞥊󠄀-󠇯]+"#
     ).unwrap();
 
     // Operator-related
@@ -137,7 +132,7 @@ lazy_static! {
         .collect();
     static ref OPERATOR_RE: Regex = Regex::new(
         format!(
-            "^{}",
+            "({})",
             {
                 let mut operator_vec = Vec::from_iter(OPERATORS.iter());
                 operator_vec.sort_by(|(op_a, _), (op_b, _)| {
@@ -161,31 +156,35 @@ lazy_static! {
     static ref COMMENT_END_RE_STR: String = escape(r"#}").to_string();
     static ref VARIABLE_START_RE_STR: String = escape(r"{{").to_string();
     static ref VARIABLE_END_RE_STR: String = escape(r"}}").to_string();
+    static ref WHITESPACE_IGNORING_CONFIG_STR: String = r"(\-|\+|)".to_string();
 
     static ref BLOCK_START_RE: Regex = Regex::new(
-        &("^".to_owned() + &BLOCK_START_RE_STR)
+        &(BLOCK_START_RE_STR.clone() + &WHITESPACE_IGNORING_CONFIG_STR)
     ).unwrap();
     static ref BLOCK_END_RE: Regex = Regex::new(
-        &("^".to_owned() + &BLOCK_END_RE_STR)
+        &(WHITESPACE_IGNORING_CONFIG_STR.clone() + &BLOCK_END_RE_STR)
     ).unwrap();
     static ref COMMENT_START_RE: Regex = Regex::new(
-        &("^".to_owned() + &COMMENT_START_RE_STR)
+        &(COMMENT_START_RE_STR.clone() + &WHITESPACE_IGNORING_CONFIG_STR)
     ).unwrap();
     static ref COMMENT_END_RE: Regex = Regex::new(
-        &("^".to_owned() + &COMMENT_END_RE_STR)
+        &COMMENT_END_RE_STR
     ).unwrap();
     static ref VARIABLE_START_RE: Regex = Regex::new(
-        &("^".to_owned() + &VARIABLE_START_RE_STR)
+        &VARIABLE_START_RE_STR
     ).unwrap();
     static ref VARIABLE_END_RE: Regex = Regex::new(
-        &("^".to_owned() + &VARIABLE_END_RE_STR)
+        &VARIABLE_END_RE_STR
+    ).unwrap();
+    static ref UNKNOWN_RE: Regex = Regex::new(
+        "."
     ).unwrap();
 
-    // should match {% raw %} and variations
+    // should search for {% raw %} and variations
     // except +%}
     static ref RAW_START_RE: Regex = Regex::new(
         format!(
-            r"(?ms)^{0}(\-|\+|)\s*raw\s*(\-|){1}",
+            r"(?ms){0}(\-|\+|)\s*raw\s*(\-|){1}",
             *BLOCK_START_RE_STR,
             *BLOCK_END_RE_STR
         ).as_ref()
@@ -193,15 +192,118 @@ lazy_static! {
     // should match {% endraw %} and variations
     static ref RAW_END_RE: Regex = Regex::new(
         format!(
-            r"(?ms)^{0}(\-|\+|)\s*endraw\s*(\+|\-|){1}",
+            r"(?ms){0}(\-|\+|)\s*endraw\s*(\+|\-|){1}",
             *BLOCK_START_RE_STR,
             *BLOCK_END_RE_STR
         ).as_ref()
     ).unwrap();
+
+    static ref EXPRESSION_RULES: Vec<Rule> = Vec::from([
+        (WHITESPACE_RE.clone(), TokenKind::Whitespace, None),
+        (FLOAT_RE.clone(), TokenKind::Float, None),
+        (INTEGER_RE.clone(), TokenKind::Integer, None),
+        (NAME_RE.clone(), TokenKind::Name, None),
+        (STRING_RE.clone(), TokenKind::String, None),
+        (OPERATOR_RE.clone(), TokenKind::Operator, None),
+    ]);
+
+    static ref RULES_BY_CONTEXT: HashMap<Context, (MatchType, Vec<Rule>)> =
+        HashMap::from([
+            (
+                Context::Root,
+                (
+                    MatchType::Search(TokenKind::Data),
+                    Vec::from([
+                        (
+                            RAW_START_RE.clone(),
+                            TokenKind::RawBegin,
+                            Some(Action::AddContext(Context::Raw)),
+                        ),
+                        (
+                            COMMENT_START_RE.clone(),
+                            TokenKind::CommentBegin,
+                            Some(Action::AddContext(Context::Comment)),
+                        ),
+                        (
+                            BLOCK_START_RE.clone(),
+                            TokenKind::BlockBegin,
+                            Some(Action::AddContext(Context::Block)),
+                        ),
+                        (
+                            VARIABLE_START_RE.clone(),
+                            TokenKind::VariableBegin,
+                            Some(Action::AddContext(Context::Variable)),
+                        ),
+                    ]),
+                ),
+            ),
+            (
+                Context::Raw,
+                (
+                    MatchType::Search(TokenKind::Data),
+                    Vec::from([(
+                        RAW_END_RE.clone(),
+                        TokenKind::RawEnd,
+                        Some(Action::PopContext),
+                    )]),
+                ),
+            ),
+            (
+                Context::Comment,
+                (
+                    MatchType::Search(TokenKind::Comment),
+                    Vec::from([(
+                        COMMENT_END_RE.clone(),
+                        TokenKind::CommentEnd,
+                        Some(Action::PopContext),
+                    )]),
+                ),
+            ),
+            (
+                Context::Block,
+                (
+                    MatchType::MatchFromStart,
+                    Vec::from_iter(
+                        [(
+                            BLOCK_END_RE.clone(),
+                            TokenKind::BlockEnd,
+                            Some(Action::PopContext),
+                        )]
+                        .into_iter()
+                        .chain(EXPRESSION_RULES.clone().into_iter())
+                        .chain([(
+                            UNKNOWN_RE.clone(),
+                            TokenKind::Error,
+                            None
+                        )]),
+                    ),
+                ),
+            ),
+            (
+                Context::Variable,
+                (
+                    MatchType::MatchFromStart,
+                    Vec::from_iter(
+                        [(
+                            VARIABLE_END_RE.clone(),
+                            TokenKind::VariableEnd,
+                            Some(Action::PopContext),
+                        )]
+                        .into_iter()
+                        .chain(EXPRESSION_RULES.clone().into_iter())
+                        .chain([(
+                            UNKNOWN_RE.clone(),
+                            TokenKind::Error,
+                            None
+                        )]),
+                    ),
+                ),
+            ),
+        ]);
 }
 
-#[derive(Debug)]
-enum BlockContext {
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+enum Context {
     Root,
     Block,
     Comment,
@@ -209,11 +311,32 @@ enum BlockContext {
     Raw,
 }
 
-/// Manages state that lexer uses to tokenize.
-/// balancing brackets shouldn't be done in the lexer
-#[derive(Debug)]
-struct TokenizeState {
-    context_stack: VecDeque<BlockContext>,
+impl Context {
+    #[cfg(test)]
+    const VALUES: [Self; 5] = [
+        Self::Root,
+        Self::Block,
+        Self::Comment,
+        Self::Variable,
+        Self::Raw,
+    ];
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Action {
+    AddContext(Context),
+    PopContext,
+}
+
+type Rule = (Regex, TokenKind, Option<Action>);
+
+enum MatchType {
+    /// Regexes should be searched for, and anything preceding the earliest
+    /// regex should be considered to have the attached `TokenKind`
+    Search(TokenKind),
+    /// Regexes should match from the beginning of the string. We should take
+    /// the regex that matches for the longest.
+    MatchFromStart,
 }
 
 /// Jinja2 lexer specifically for dbt
@@ -244,16 +367,84 @@ struct TokenizeState {
 struct Lexer {}
 
 impl Lexer {
-    fn tokenize<'a>(&self, input: &'a str) -> Vec<Token> {
-        let state = TokenizeState {
-            context_stack: VecDeque::from([BlockContext::Root]),
-        };
-
-        todo!()
+    pub fn new() -> Self {
+        Lexer {}
     }
 
-    fn next_token(&self, input: &str) -> Token {
-        todo!()
+    pub fn tokenize<'a>(&self, input: &'a str) -> Vec<Token> {
+        let mut context_stack = VecDeque::from([Context::Root]);
+        let mut current_idx = 0;
+        let mut token_stream = Vec::new();
+        while current_idx != input.len() {
+            let current_context = context_stack
+                .back()
+                .expect("Lexer failed due to empty context");
+            let (match_type, rules) = RULES_BY_CONTEXT.get(current_context).unwrap();
+            let matches = rules.iter().enumerate().filter_map(|(i, (reg, _, _))| {
+                let reg_match = reg
+                    .find_from_pos(input, current_idx)
+                    .expect("regex find failed, likely because lookaheads are too complex")?;
+                Some((i, reg_match.start(), reg_match.end()))
+            });
+            let (next_idx, maybe_action) = match match_type {
+                MatchType::Search(skipped_token_kind) => {
+                    match matches.min_by_key(|&(i, start_idx, _)| (start_idx, i)) {
+                        Some((min_i, min_start, end_idx)) => {
+                            if min_start != current_idx {
+                                token_stream.push(Token {
+                                    kind: *skipped_token_kind,
+                                    len: min_start - current_idx,
+                                });
+                            }
+                            let (_, token_kind, action) = rules[min_i];
+                            token_stream.push(Token {
+                                kind: token_kind,
+                                len: end_idx - min_start,
+                            });
+
+                            (end_idx, action)
+                        }
+                        None => {
+                            token_stream.push(Token {
+                                kind: *skipped_token_kind,
+                                len: input.len() - current_idx,
+                            });
+                            (input.len(), None)
+                        }
+                    }
+                }
+                MatchType::MatchFromStart => {
+                    match matches
+                        .filter(|&(_, start_idx, _)| start_idx == current_idx)
+                        .max_by_key(|&(i, _, end_idx)| (end_idx, -(i as i64)))
+                    {
+                        Some((min_i, _, end_idx)) => {
+                            let (_, token_kind, action) = rules[min_i];
+                            token_stream.push(Token {
+                                kind: token_kind,
+                                len: end_idx - current_idx,
+                            });
+                            (end_idx, action)
+                        }
+                        None => {
+                            // couldn't find anything hmm
+                            unreachable!("UNKNOWN_RE should have caught the error")
+                        }
+                    }
+                }
+            };
+            // update state
+            if let Some(action) = maybe_action {
+                match action {
+                    Action::AddContext(ctx) => context_stack.push_back(ctx),
+                    Action::PopContext => {
+                        context_stack.pop_back();
+                    }
+                }
+            }
+            current_idx = next_idx;
+        }
+        token_stream
     }
 }
 
@@ -300,15 +491,24 @@ mod tests {
     }
 
     #[test]
-    fn test_raw_start_regex() {
-        assert_eq!(RAW_START_RE.is_match("{% raw %}").unwrap(), true);
+    fn test_raw_start_regex_search() {
+        assert_eq!(
+            RAW_START_RE.find("{% raw %}").unwrap().map(|m| m.start()),
+            Some(0)
+        );
+        assert_eq!(
+            RAW_START_RE
+                .find("blank {% raw %}")
+                .unwrap()
+                .map(|m| m.start()),
+            Some(6)
+        );
         assert_eq!(RAW_START_RE.is_match("{%- raw %}").unwrap(), true);
         assert_eq!(RAW_START_RE.is_match("{%+ raw %}").unwrap(), true);
         assert_eq!(RAW_START_RE.is_match("{% raw -%}").unwrap(), true);
         assert_eq!(RAW_START_RE.is_match("{% raw +%}").unwrap(), false);
         assert_eq!(RAW_START_RE.is_match("{% \n raw %}").unwrap(), true);
         assert_eq!(RAW_START_RE.is_match("{% raw \n %}").unwrap(), true);
-        assert_eq!(RAW_START_RE.is_match("{% \n raw \n %}").unwrap(), true);
         assert_eq!(RAW_START_RE.is_match("{%- raw -%}").unwrap(), true);
         assert_eq!(RAW_START_RE.is_match("{%+ raw -%}").unwrap(), true);
         assert_eq!(RAW_START_RE.is_match("{%+ raw +%}").unwrap(), false);
@@ -317,6 +517,17 @@ mod tests {
 
     #[test]
     fn test_raw_end_regex() {
+        assert_eq!(
+            RAW_END_RE.find("{% endraw %}").unwrap().map(|m| m.start()),
+            Some(0)
+        );
+        assert_eq!(
+            RAW_END_RE
+                .find("blank {% endraw %}")
+                .unwrap()
+                .map(|m| m.start()),
+            Some(6)
+        );
         assert_eq!(RAW_END_RE.is_match("{% endraw %}").unwrap(), true);
         assert_eq!(RAW_END_RE.is_match("{%- endraw %}").unwrap(), true);
         assert_eq!(RAW_END_RE.is_match("{%+ endraw %}").unwrap(), true);
@@ -329,5 +540,49 @@ mod tests {
         assert_eq!(RAW_END_RE.is_match("{%+ endraw -%}").unwrap(), true);
         assert_eq!(RAW_END_RE.is_match("{%+ endraw +%}").unwrap(), true);
         assert_eq!(RAW_END_RE.is_match("{%+ endraw +%}").unwrap(), true);
+    }
+
+    #[test]
+    fn test_rules_by_context_complete() {
+        Context::VALUES.iter().for_each(|c| {
+            RULES_BY_CONTEXT
+                .get(c)
+                .expect(&format!("Failed to find rules for context {:?}", c));
+        })
+    }
+
+    fn print_tokenized(input: &str, tokens: &Vec<Token>) {
+        let (_, to_print) = tokens.iter().fold((0, "".to_owned()), |(idx, s), token| {
+            let next_idx = idx + token.len;
+            let s = format!("{}\n({:?}\t {:?})", s, &input[idx..next_idx], token.kind);
+            (next_idx, s)
+        });
+        println!("[{}\n]", to_print);
+    }
+
+    #[test]
+    fn test_tokenize() {
+        let lexer = Lexer::new();
+        let inputs = [
+            "{%+\n set something = 1 * 2 %}\n{{ something }}",
+            "{% raw %}\n{{ something }} {% endraw %}",
+            "{% raw %}\n{{ something }}",
+            "{% ( %} {% ) %}",
+            "{% { %} {% } %}",
+            "{% {{ test }}",
+            "{% set x = {{ test }}",
+            "{{ test {% x",
+            "{{ foo.0.0 }}",
+            r#"{{ config(
+                    materialized="table",
+                    meta={
+                        "something": "another", 
+                    }
+                ) }}"#,
+        ];
+        for input in inputs {
+            let tokens = lexer.tokenize(input);
+            print_tokenized(input, &tokens)
+        }
     }
 }
