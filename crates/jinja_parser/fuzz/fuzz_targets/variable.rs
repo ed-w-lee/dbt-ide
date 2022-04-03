397da671a1636ba1c43aa8f3bff5a18f078a6af7
Edward Lee <edwardlee.259@gmail.com>
2022-04-03 15:21:01 -0700

    move fuzzer around, and rename

--- crates/jinja_parser/fuzz/fuzz_targets/variable.rs
+++ crates/jinja_parser/fuzz/fuzz_targets/variable.rs
@@ -1,9 +1,9 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-extern crate dbt_ide;
+extern crate dbt_jinja_parser;
 
-use dbt_ide::dbt_jinja2::{lexer::tokenize, parser::parse};
+use dbt_jinja_parser::{lexer::tokenize, parser::parse};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
